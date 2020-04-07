2eafc3b3e3a8d2a5c6276fa6ae181cdd2cb99849
github username <github email address>
2020-04-06 22:26:05 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -27,13 +27,29 @@ class Song
     @@count
   end
 
-  # def self.genre_count
+  def self.genre_count
+    genres_hash = {}
+    @@genres.each do |genre|
+      if !genres_hash.has_key?(genre)
+        genres_hash[genre] = 1
+      else
+        genres_hash[genre] += 1
+      end
+    end
+    genres_hash
+  end
 
-  # end
 
-  # def artist_count
-
-  # end
-
+  def self.artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      if !artist_hash.has_key?(artist)
+        artist_hash[artist] = 1
+      else
+        artist_hash[artist] += 1
+      end
+    end
+    artist_hash
+  end
 
 end
